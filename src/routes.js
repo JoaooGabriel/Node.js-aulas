910fc8ce92b25ea64989598106990a91fc03fb0e
Joao-gabriel-15 <caetanojoaogabriel@gmail.com>
2020-01-19 20:11:46 -0300

    Alteretion routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 const {Router} = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
@@ -10,9 +12,24 @@ const routes = Router();
 //Route Params-> request.params (Identificar um recurso na alteração ou remoção)
 //Body-> request.body (Dados para a criação ou alteração de um resgistro)
 
-routes.post('/devs', (request, response) => {
-    console.log('request.body')
-    return response.json({ message: "Hello OMnistack" });
+routes.post('/devs', async (request, response) => {
+    const { github_username } = request.body; //Fazendo a busca direto no GitHub
+
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    const { name = login, avatar_url, bio } = (apiResponse.data); //Buscando as informações do Github e traznedo para a API
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+    })
+
+    return response.json(dev);
 });
 
 module.exports = routes;
